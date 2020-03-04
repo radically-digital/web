01fb74b795464f316485d348a79cbb4b2153341b
Piotr Lewandowski <contact@piotrlewandowski.info>
2020-03-04 12:07:47 +0000

    feat: Blog page with list of posts

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 
-const Blog = () => (
-  <Layout
-    title="Radically Digital - Blog"
-    description="Find out more about Blog at Radically Digital"
-  >
-    <h2>Blog</h2>
-  </Layout>
-);
+const Blog = ({ data: { allPrismicPost } }) => {
+
+  return (
+    <Layout
+      title="Radically Digital - Blog"
+      description="Find out more about Blog at Radically Digital"
+    >
+      <h2>Blog</h2>
+      <ul>
+        {allPrismicPost.edges.map((edge) => {
+          return (
+            <li key={edge.node.id}>
+              <Link to={`/blog/${edge.node.uid}`}>
+                {edge.node.data.title.text}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </Layout>
+  )
+};
+
+export const pageQuery = graphql`
+  query blogPageQuery {
+    allPrismicPost {
+      edges {
+        node {
+          id
+          uid
+          data {
+            title {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Blog;
