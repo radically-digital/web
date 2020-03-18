824911124d141ae47d5fffad7a13f86aafc2e907
Piotr Lewandowski <contact@piotrlewandowski.info>
2020-03-18 15:49:48 +0000

    refactor: Site Header restyling

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import logo from '../assets/images/radically-digital-logo.jpg';
+import RDLogo from '../assets/logos/lock-up_wide-gradient.svg';
 
 import Nav from './Nav';
 
 const SiteHeader = () => (
   <header className="site-header">
     <Link to="/">
-      <img className="site-header__logo" src={logo} alt="Radically Digital logo" />
+      <img className="site-header__logo" src={RDLogo} alt="Radically Digital logo" />
     </Link>
 
     <Nav/>
