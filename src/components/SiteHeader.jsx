780dbe934e85a3438a5f24de2baed15cecb9c176
Piotr Lewandowski <contact@piotrlewandowski.info>
2020-03-20 10:57:47 +0000

    feat: Hamburger menu

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 
 import RDLogo from '../assets/logos/lock-up_wide-gradient.svg';
 
 import Nav from './Nav';
+import NavButton from './NavButton';
+const SiteHeader = () => {
+  const [open, setOpen] = useState(false);
 
-const SiteHeader = () => (
-  <header className="site-header">
-    <Link to="/">
-      <img className="site-header__logo" src={RDLogo} alt="Radically Digital logo" />
-    </Link>
+  return (
+    <header className="site-header">
+      <div className="site-header__toolbar">
+        <Link to="/">
+          <img className="site-header__logo" src={RDLogo} alt="Radically Digital logo" />
+        </Link>
 
-    <Nav/>
-  </header>
-);
+        <NavButton open={open} setOpen={setOpen} />
+      </div>
+
+      <Nav open={open} />
+    </header>
+  )
+};
 
 export default SiteHeader;
