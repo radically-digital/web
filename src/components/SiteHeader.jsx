78f36b9efeaaae435fd3ecee33a67d23b0e082d7
Oliver Smit <olmesm@gmail.com>
2020-03-27 09:27:23 +0000

    fix header link

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,26 +1,47 @@
 import React, { useState } from 'react';
-import { Link } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import RDLogo from '../assets/logos/lock-up_wide-gradient.svg';
 
 import Nav from './Nav';
 import NavButton from './NavButton';
+
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        urlMain
+        urlTransitioning
+      }
+    }
+  }
+`;
+
 const SiteHeader = () => {
   const [open, setOpen] = useState(false);
+  const {
+    site: {
+      siteMetadata: { urlMain },
+    },
+  } = useStaticQuery(query);
 
   return (
     <header className="site-header">
       <div className="site-header__toolbar">
-        <Link to="/">
-          <img className="site-header__logo" src={RDLogo} alt="Radically Digital logo" />
-        </Link>
+        <a href={urlMain}>
+          <img
+            className="site-header__logo"
+            src={RDLogo}
+            alt="Radically Digital logo"
+          />
+        </a>
 
         <NavButton open={open} setOpen={setOpen} />
       </div>
 
       <Nav open={open} />
     </header>
-  )
+  );
 };
 
 export default SiteHeader;
