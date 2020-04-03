50e32ecb96f64d42a15ad96e12bf03d21a251431
Oliver Smit <olmesm@gmail.com>
2020-04-03 17:29:30 +0100

    added questions

--- src/pages/quote.jsx
+++ src/pages/quote.jsx
@@ -5,13 +5,47 @@ import Layout from '../components/Layout';
 
 const questions = [
   {
-    question: "What's your name?",
-    type: 'text',
+    type: 'button',
+    options: ['Yes'],
+    question:
+      'In under 60 seconds and just a few Qs, you can help us understand your challenge. Sound good?',
   },
   {
-    question: 'Which City are you looking in?',
+    question: 'What is your industry?',
     type: 'checkbox',
-    option: ['London', 'New York'],
+    options: [
+      'Retail',
+      'Financial Services',
+      'Technology, Healthcare',
+      'Energy/Utilities',
+      'Mining',
+      'Food/Drinks',
+      'Automotive',
+      'Real Estate',
+      'Manufacturing',
+      'Education',
+      'Tourism',
+      'Media',
+    ],
+  },
+  {
+    type: 'button',
+    question:
+      'What challenge can we help with? Feel free to select multiple options from the selection below ',
+  },
+  { type: 'button', question: 'Any Specific Project Goals? ' },
+  {
+    type: 'button',
+    question: `e.g. Building a new product\nStream line operations\nAgile Coaching\netc`,
+  },
+  {
+    type: 'button',
+    question: 'That all sounds great! How soon are you looking to start?',
+  },
+  {
+    type: 'text',
+    question:
+      'Excellent a [dependent on answer selection in Q2] coming your way. Enter your email address and we will send your options over! ',
   },
 ];
 
