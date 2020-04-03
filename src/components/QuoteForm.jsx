ea2c56df7ff9f4893bf18e887e0dd999747e601e
James Grant <24829541+jamsgra@users.noreply.github.com>
2020-04-03 17:57:27 +0100

    Post request onSubmit

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -4,19 +4,40 @@ const CheckboxComponent = () => <p>Checkbox.</p>;
 const TextComponent = () => <p>Text.</p>;
 
 const typeMapping = {
-  checkbox: <CheckboxComponent/>,
-  text: <TextComponent/>
+  checkbox: <CheckboxComponent />,
+  text: <TextComponent />,
 };
 
 const QuoteForm = ({ questions }) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [formState, setFormState] = useState({});
   const currentQuestion = questions[currentQuestionIndex];
-  const Component = () => typeMapping[currentQuestion.type];
+  const Component = typeMapping[currentQuestion.type];
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const answersKeys = Object.keys(formState);
+    if (!answersKeys.length) {
+      return;
+    }
+    const fetchParams = {
+      method: 'POST',
+      body: JSON.stringify(formState),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    fetch(
+      'https://1un8zjny3m.execute-api.us-east-1.amazonaws.com/dev/static-site-mailer',
+      fetchParams
+    ).then(res => {
+      return res;
+    });
+  };
+
   return (
-    <form>
-      <input>
-      </input>
-      <Component/>
+    <form onSubmit={handleSubmit}>
+      <input type="submit" value="Submit" />
     </form>
   );
 };
