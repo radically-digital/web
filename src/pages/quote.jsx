bc9546935c1cb7250529b47408a39545d2dcc6fb
Oliver Smit <olmesm@gmail.com>
2020-04-03 17:01:02 +0100

    removed class

--- src/pages/quote.jsx
+++ src/pages/quote.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 
 const Quote = () => (
-  <Layout
-    pageClass="join-us"
-    title="Radically Digital - Quote"
-    description="Get a Quote"
-  >
+  <Layout title="Radically Digital - Quote" description="Get a Quote">
     <section className="page-title">
       <div className="page-title__container">
         <h1 className="page-title__heading">Quote</h1>
