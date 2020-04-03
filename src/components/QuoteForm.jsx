68074f64fcadc109ac3a3155e46a96abbdb18716
Oliver Smit <olmesm@gmail.com>
2020-04-03 17:57:39 +0100

    added a dynamic form

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -1,22 +1,44 @@
+/* eslint-disable no-console */
 import React, { useState } from 'react';
 
-const CheckboxComponent = () => <p>Checkbox.</p>;
-const TextComponent = () => <p>Text.</p>;
+// const CheckboxComponent = () => <p>Checkbox.</p>;
+// const TextComponent = () => <p>Text.</p>;
 
-const typeMapping = {
-  checkbox: <CheckboxComponent/>,
-  text: <TextComponent/>
-};
+// const typeMapping = {
+//   checkbox: <CheckboxComponent />,
+//   text: <TextComponent />,
+// };
 
 const QuoteForm = ({ questions }) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const currentQuestion = questions[currentQuestionIndex];
-  const Component = () => typeMapping[currentQuestion.type];
+  // const Component = () => typeMapping[currentQuestion.type];
+  const [formState, setFormState] = useState({});
+
   return (
-    <form>
-      <input>
-      </input>
-      <Component/>
+    <form
+      onSubmit={e => {
+        e.preventDefault() || console.log(formState);
+      }}
+    >
+      <label>
+        {currentQuestion.question}
+        <input
+          onChange={e => {
+            setFormState({
+              ...formState,
+              [`question_${[currentQuestionIndex]}`]: e.target.value,
+            });
+          }}
+        />
+
+        <button
+          onClick={() => setCurrentQuestionIndex(currentQuestionIndex + 1)}
+        >
+          Progress
+        </button>
+      </label>
+      {/* <Component /> */}
     </form>
   );
 };
