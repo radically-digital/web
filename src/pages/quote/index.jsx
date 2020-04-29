8eee5a7f27c20febf18589f7cf0113ef2ad840da
Oliver Smit <olmesm@gmail.com>
2020-04-29 09:26:17 +0100

    fixed import error

--- src/pages/quote/index.jsx
+++ src/pages/quote/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../../components/Layout"
 import Hero from "../../components/Hero"
-import { QuoteForm } from "./QuoteForm"
+import { QuoteForm } from "../../components/QuoteForm"
 
 const Quote = () => (
   <Layout
