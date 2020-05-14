93bebb06704433b3d0d0ba068edad2217a820517
James Grant <24829541+jamsgra@users.noreply.github.com>
2020-05-14 16:41:03 +0100

    Check type, add image & caption, styles

--- src/components/ArticleSections/ArticleSections.jsx
+++ src/components/ArticleSections/ArticleSections.jsx
@@ -3,31 +3,57 @@ import { v4 as uuidv4 } from "uuid"
 
 import { styles } from "./styles"
 
-const { IntroParagraph, ParagraphHeading } = styles
+const {
+  IntroParagraph,
+  ParagraphSection,
+  ParagraphHeading,
+  ImageContainer,
+  Image,
+  ImageCaption,
+} = styles
 
 const ArticleSections = ({ sections }) => {
   const sectionsMarkup = sections.map((sectionItem) => {
-    const item = sectionItem.primary
+    const { type } = sectionItem
 
-    if (item.intro_paragraph) {
-      return (
-        <IntroParagraph key={uuidv4()}>{item.intro_paragraph}</IntroParagraph>
-      )
+    if (type === "intro_paragraph") {
+      const { intro_paragraph } = sectionItem.primary
+      return <IntroParagraph key={uuidv4()}>{intro_paragraph}</IntroParagraph>
     }
 
-    if (item.text_section) {
-      const paragraphs = item.text_section.map((paragraph) => (
+    if (type === "text_section") {
+      const { text_section } = sectionItem.primary
+      const paragraphs = text_section.map((paragraph) => (
         <p key={uuidv4()}>{paragraph.text}</p>
       ))
 
-      return <section key={uuidv4()}>{paragraphs}</section>
+      return <ParagraphSection key={uuidv4()}>{paragraphs}</ParagraphSection>
     }
 
-    if (item.paragraph_heading) {
+    if (type === "heading") {
+      const { paragraph_heading } = sectionItem.primary
       return (
-        <ParagraphHeading key={uuidv4()}>
-          {item.paragraph_heading}
-        </ParagraphHeading>
+        <ParagraphHeading key={uuidv4()}>{paragraph_heading}</ParagraphHeading>
+      )
+    }
+
+    if (type === "image") {
+      const {
+        imageSharp,
+        image_caption,
+        full_width,
+        image,
+      } = sectionItem.fields[0]
+
+      return (
+        <ImageContainer key={uuidv4()}>
+          <Image
+            fluid={imageSharp.childImageSharp.fluid}
+            alt={image.alt}
+            fullWidth={full_width}
+          />
+          <ImageCaption>{image_caption}</ImageCaption>
+        </ImageContainer>
       )
     }
 
--- src/components/ArticleSections/styles.jsx
+++ src/components/ArticleSections/styles.jsx
@@ -1,15 +1,31 @@
 import { theme } from "../../styles/theme"
 import { styled } from "linaria/react"
 import { breakpoints } from "../../styles/theme/breakpoints"
+import Img from "gatsby-image"
 
 const { spacing, fonts } = theme
+
+const textMargins = `
+    margin: 0 ${spacing(5)};
+    margin-bottom: ${spacing(10)};
+
+    ${breakpoints.md} {
+      margin: 0 ${spacing(40)};
+      margin-bottom: ${spacing(10)};
+    }
+    ${breakpoints.xl} {
+      margin: 0 ${spacing(100)};
+      margin-bottom: ${spacing(10)};
+    }
+`
 
 export const styles = {
   IntroParagraph: styled.section`
     font-size: ${fonts.fontSize.paragraph.lg};
     line-height: ${fonts.lineHeight.paragraph.xl};
     font-weight: ${fonts.fontWeight.bold};
-    margin-bottom: ${spacing(10)};
+
+    ${textMargins}
 
     ${breakpoints.xs} {
       font-size: ${fonts.fontSize.paragraph.xl};
@@ -21,11 +37,37 @@ export const styles = {
       margin-top: 0.15em;
     }
   `,
+  ParagraphSection: styled.section`
+    margin-bottom: ${spacing(10)};
+    ${textMargins}
+  `,
   ParagraphHeading: styled.h2`
     font-size: ${fonts.fontSize.heading.xs};
-    margin: ${spacing(10)} 0;
+    ${textMargins}
+
     ${breakpoints.xs} {
       font-size: ${fonts.fontSize.heading.sm};
+    },
+  `,
+  ImageContainer: styled.div`
+    margin-bottom: ${spacing(10)};
+  `,
+  Image: styled(Img)`
+    margin: 0;
+    margin-bottom: ${spacing(5)};
+
+    ${breakpoints.lg} {
+      margin: 0 ${(props) => (props.fullWidth ? 0 : spacing(30))};
+      margin-bottom: ${spacing(5)};
     }
+    ${breakpoints.xl} {
+      margin: 0 ${(props) => (props.fullWidth ? 0 : spacing(70))};
+      margin-bottom: ${spacing(5)};
+    }
+  `,
+  ImageCaption: styled.p`
+    ${textMargins}
+    font-size: ${fonts.fontSize.paragraph.sm}
+    opacity: 59%
   `,
 }
