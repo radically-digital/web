84a3aa25400b80970bb6aec224ebc308a5d7ae74
James Grant <24829541+jamsgra@users.noreply.github.com>
2020-05-20 12:00:44 +0100

    Separate line

--- src/components/ArticleHeader/styles.jsx
+++ src/components/ArticleHeader/styles.jsx
@@ -10,7 +10,8 @@ export const styles = {
     font-weight: ${fonts.fontWeight.bold};
     text-align: center;
     margin: ${spacing(4)};
-    line-height: ${fonts.lineHeight.heading.xl} ${breakpoints.xs} {
+    line-height: ${fonts.lineHeight.heading.xl};
+    ${breakpoints.xs} {
       font-size: ${fonts.fontSize.heading.sm};
     }
     ${breakpoints.sm} {
